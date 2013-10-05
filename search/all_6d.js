b354730efe628398e7b880591e4896941063c7fc
Mario S. Könz <mskoenz@gmx.net>
2013-10-05 13:07:44 +0200

    3. Commit: added mainpage stuff

--- search/all_6d.js
+++ search/all_6d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['main',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
-  ['main_2ecpp',['main.cpp',['../main_8cpp.html',1,'']]]
+  ['main_2ecpp',['main.cpp',['../main_8cpp.html',1,'']]],
+  ['mainpage_2edox',['mainpage.dox',['../mainpage_8dox.html',1,'']]]
 ];
